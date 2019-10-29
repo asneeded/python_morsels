bbce1dd64a3e37e563ee38ad512a6496f61a0f38
asneeded <adamdsneed@gmail.com>
2019-10-28 21:39:13 -0500

    added __eq__

--- point.py
+++ point.py
@@ -9,9 +9,16 @@ class Point:
 
     def __repr__(self):
         return f"Point(x:{self.x}, y:{self.y}, z:{self.z})"
-    
+
+    def __eq__(self, other):
+        # return self.x == other.x and self.y == other.y and self.z == other.z
+        return (self.x, self.y, self.z) == (other.x, other.y, other.z)
 
 
 if __name__ == '__main__':
     p = Point(3,5,7)
     print(p)
+    p1 = Point(5,6,8)
+    print(p1)
+    p3 = p1 + p
+    print(p3)
