0c24bbef95e1fde0ff377b0b9f5092f016ad55ed
asneeded <adamdsneed@gmail.com>
2019-10-29 16:38:26 -0500

    added __add__,__sub__, __mul__, __rmul__

--- point.py
+++ point.py
@@ -8,11 +8,25 @@ class Point:
         self.z = z
 
     def __repr__(self):
-        return f"Point(x:{self.x}, y:{self.y}, z:{self.z})"
+        return f"Point(x={self.x}, y={self.y}, z={self.z})"
 
     def __eq__(self, other):
         # return self.x == other.x and self.y == other.y and self.z == other.z
         return (self.x, self.y, self.z) == (other.x, other.y, other.z)
+
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y, self.z - other.z)
+    
+
+    def __mul__(self, other):
+        return Point(self.x * other , self.y * other , self.z * other)
+
+    def __rmul__(self, other):
+        return Point(other * self.x, other * self.y, other * self.z)
 
 
 if __name__ == '__main__':
@@ -22,3 +36,5 @@ if __name__ == '__main__':
     print(p1)
     p3 = p1 + p
     print(p3)
+    p.x=10
+    print(p, p3)
